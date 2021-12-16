ed4171c2ba4b27ffc0d8f8cf3fad5a5f47ee42c4
zaraathifa <64517490+zaraathifa@users.noreply.github.com>
2021-12-16 23:04:44 +0800

    Update register.php
    Add new function

--- MYPrivateVaccine/register.php
+++ MYPrivateVaccine/register.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
+
     <link rel="icon" href="img/assets/favicon.png" type="img/png">
     <title>MY Private Vaccine - SignUp</title>
 
@@ -21,7 +22,7 @@
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
 
     <style>
-        #hidden_div, #hidden_div2 {
+        #hidden_div, #hidden_div2, #hidden_div3, #hidden_div4 {
             display: none;
         }
     </style>
@@ -43,8 +44,8 @@
                         if(isset($_GET['message'])){
                             if($_GET['message'] == "fail"){
                                 echo "<center><div class='alert alert-danger' role='alert'>Username have been used!</div><center>";
-                            } else{
-                                echo "<center><div class='alert alert-danger' role='alert'>Failed saving the data! Try again.</div><center>";
+                            } elseif ($_GET['message'] == "success-add") {
+                                echo "<center><div class='alert alert-success' role='alert'>Success Add New Healthcare</div><center>";
                             }
                         } ?>
                         <form class="user" action="asset/register-user.php" method="POST">
@@ -57,32 +58,45 @@
                             </div>
                             <div class="form-group ">
                                 <input type="text" class="form-control form-control-user" id="exampleFirstName"
-                                       placeholder="Username" name="username" required="required">
+                                       name="username" placeholder="Username"  required="required">
                             </div>
+
                             <div class="form-group">
                                 <input type="password" class="form-control form-control-user"
-                                       id="exampleInputPassword" placeholder="Password" name="password" required="required">
+                                       id="exampleInputPassword" name="password" placeholder="Password"  required="required">
                             </div>
                             <div class="form-group">
                                 <input type="email" class="form-control form-control-user" id="exampleInputEmail"
-                                       placeholder="Email Address" name="email" required="required">
+                                       name="email" placeholder="Email Address"  required="required">
                             </div>
                             <div class="form-group">
                                 <input type="text" class="form-control form-control-user" id="exampleFirstName"
-                                       placeholder="Full Name" name="fullname" required="required">
+                                       name="fullname" placeholder="Full Name"  required="required">
                             </div>
                             <div id="hidden_div" class="form-group">
                                 <input id="ICPassport" type="text" class="form-control form-control-user" name="icpassport"
-                                       placeholder="ICPassport" required="required">
+                                       placeholder="ICPassport">
                             </div>
-                            <div id="hidden_div2" class="form-group">
-                                <select name="centreName" class="form-control">
-                                    <option disabled selected>--- Centre Name ---</option>
-                                    <option value="MSU Medical Centre">MSU Medical Centre[Selangor]</option>
-                                    <option value="Senawang Specialist Hospital">Senawang Specialist Hospital[Negri Sembilan]</option>
-                                    <option value="Timberland Medical Centre">Timberland Medical Centre[Serawak]</option>
-                                </select>
+								<div id="hidden_div2" class="form-group">
+								<select name="centreName" class="form-control" onchange="showDiv2(this)">
+                                    <option value="#" disabled selected>--- Centre Name ---</option>
+									<?php
+										include "asset/connection.php";  // Using database connection file here
+										$records = mysqli_query($connect, "SELECT * FROM healthcarecentre ");  // Use select query here
+										while($data = mysqli_fetch_array($records))
+										{
+											echo "<option value='". $data['centreName'] ."'>" .$data['centreName'] ."
+                                            <a>/</a>".$data['address'] ."</option>";  // displaying data in option menu
+										}?>
+                                    <option value="other">Other</option>
+								</select>
                             </div>
+							<div id="hidden_div3">
+                                <div class="form-group">
+                                    <a class="btn btn-primary" href="asset/new-healthcare/create.php">Add Healthcare</a>
+                                </div>
+                            </div>
+
                             <button type="submit" name="submit" class="btn btn-primary btn-user btn-block">Register Account</button>
                         </form>
                         <hr>
@@ -119,6 +133,14 @@
             document.getElementById('hidden_div2').style.display = "block";
         } else{
             document.getElementById('hidden_div2').style.display = "none";
+        }
+    }
+	
+	function showDiv2(select){
+        if(select.value=='other'){
+            document.getElementById('hidden_div3').style.display = "block";
+        } else{
+            document.getElementById('hidden_div3').style.display = "none";
         }
     }
 </script>
